87b1f62760eb8e31c3803f8192bfe7b4faea6124
“alerenna” <alessandrorennafm@gmail.com>
2025-07-04 10:44:10 +0200

    Milestone 2: Implements optimized search for the list of politicians

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 
 
 function App() {
   const [politicians, setPoliticians] = useState([])
+  const [search, setSearch] = useState("")
 
   useEffect(() => {
     fetch("http://localhost:3333/politicians")
@@ -13,11 +14,29 @@ function App() {
 
   console.log(politicians)
 
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter(politician => {
+      const perName = politician.name.toLowerCase().includes(search.toLowerCase())
+      const perBio = politician.biography.toLowerCase().includes(search.toLowerCase())
+
+      return perName || perBio
+
+    })
+  }, [politicians, search])
+
+
   return (
     <div className='container'>
       <h1>Lista politici</h1>
       <div className="row gap-3 politicians-list">
-        {politicians.map(politician => (
+        <h3>Cerca un politico</h3>
+        <input
+          type="text"
+          placeholder='Cerca un politico per nome o bio'
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+        {filteredPoliticians.map(politician => (
           <div className="col-md-3 mb-4" key={politician.id}>
             <div className="card" style={{ width: "18rem" }}>
               <img className='card-img-top' src={politician.image} alt={politician.name} />
@@ -36,3 +55,15 @@ function App() {
 
 
 export default App
+
+
+/* 
+
+📌 Milestone 2: Implementare la ricerca ottimizzata
+- Aggiungi un campo di ricerca (<input type="text">) sopra la lista dei politici.
+- Permetti all’utente di filtrare i risultati in base a nome o biografia (se il testo cercato è incluso). Suggerimento: Creare un array derivato filtrato, che viene aggiornato solo quando cambia la lista di politici o il valore della ricerca.
+- ❌ Non usare useEffect per aggiornare l’array filtrato.
+
+Obiettivo: Migliorare le prestazioni evitando ricalcoli inutili quando il valore della ricerca non cambia.
+
+*/
